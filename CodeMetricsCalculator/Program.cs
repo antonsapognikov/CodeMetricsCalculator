3fadd917f181cef826d4910702324740ee1aec8a
RavingRabbit <RavingDev@gmail.com>
2013-10-28 01:39:09 +0300

    Java method parsing example added.

--- CodeMetricsCalculator/Program.cs
+++ CodeMetricsCalculator/Program.cs
@@ -10,47 +10,10 @@ namespace CodeMetricsCalculator
     {
         private static void Main(string[] args)
         {
-            const string source = "for (int j = 0; !(j < buttons.length); j++)" +
-                                  "for (int k = 0; (k < buttons[j].length) || !(j < buttons.length); k++)" +
-                                  "for (int m = 0; m < alternativeNames.length; m++)" +
-                                  "if (buttons[j][k].getText().equals(alternativeNames[m][0]))" +
-                                  "buttons[j][k].setText(alternativeNames[m][1]);";
-            var javaExpression = new JavaExpression(source); //вообще говоря, тут надо создавать JavaMethodBody
-            //и парсить его на JavaExpression's... но пока парсинг экспрешшенов из боди не реализован
-
-            var operators = javaExpression.GetOperators();
-
-            /*Console.WriteLine("Original source: ");
-            Console.WriteLine(source);
-            Console.WriteLine();
-            Console.WriteLine("Normolized source:");
-            Console.WriteLine(javaExpression.NormalizedSource);*/
-            Console.WriteLine();
-            Console.WriteLine("#########################################");
-            Console.WriteLine();
-            Console.WriteLine("Operators: ");
-            foreach (var operatorInfo in operators)
-            {
-                Console.WriteLine("{0} ({1}, {2}) {3} - {4}",
-                                  operatorInfo.Key.GetType().Name,
-                                  operatorInfo.Key.OperationType,
-                                  operatorInfo.Key.Syntax,
-                                  operatorInfo.Key.Name,
-                                  operatorInfo.Value);
-            }
-
             var classesSource = Resource.TestJavaCode;
             var classesCode = new JavaCode(classesSource);
             var classes = new JavaClassParser().Parse(classesCode);
 
-            /*Console.WriteLine("Original source: ");
-            Console.WriteLine(classesSource);
-            Console.WriteLine();
-            Console.WriteLine("Normolized source:");
-            Console.WriteLine(classesCode.NormalizedSource);*/
-            Console.WriteLine();
-            Console.WriteLine("#########################################");
-            Console.WriteLine();
             Console.WriteLine("Classes: ");
             foreach (var classInfo in classes)
             {
@@ -58,6 +21,16 @@ namespace CodeMetricsCalculator
                                   classInfo.Name,
                                   Environment.NewLine,
                                   classInfo.NormalizedSource);
+                Console.WriteLine("***************************");
+                Console.WriteLine("Methods:");
+                foreach (var methodInfo in classInfo.GetMethods())
+                {
+                    Console.WriteLine("Name - {0}{1}Sources:{1}{2}",
+                                      methodInfo.Name,
+                                      Environment.NewLine,
+                                      methodInfo.NormalizedSource);
+                    Console.WriteLine("***************************");
+                }
             }
 
             Console.ReadKey();
