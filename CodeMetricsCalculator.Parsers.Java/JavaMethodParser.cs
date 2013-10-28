f1db01b742f028d96a5902b5cccd3dd5e6bfde4d
RavingRabbit <RavingDev@gmail.com>
2013-10-28 22:34:17 +0300

    JavaMethodParser improved (parsing method parameters).

--- CodeMetricsCalculator.Parsers.Java/JavaMethodParser.cs
+++ CodeMetricsCalculator.Parsers.Java/JavaMethodParser.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using CodeMetricsCalculator.Parsers.CodeInfo;
 using CodeMetricsCalculator.Parsers.Exceptions;
 using CodeMetricsCalculator.Parsers.Java.CodeInfo;
 
@@ -12,43 +15,65 @@ namespace CodeMetricsCalculator.Parsers.Java
     internal class JavaMethodParser : JavaCodeParser<JavaClass, IReadOnlyCollection<JavaMethod>>,
         IMethodParser<JavaClass, JavaMethod>
     {
+        //{return type} {method name} ({parameter})
+        private const string MethodRegexString = @"[a-zA-Z][a-zA-Z1-9<>]+ +([a-zA-Z][a-zA-Z1-9]+) *" +
+                                                 @"\(([a-zA-Z1-9<> ,]*)\) *{";
+
         static JavaMethodParser()
         {
-            //{return type} {method name} (..
             //todo add generic method support
-            MethodNameRegex = new Regex(@"[a-zA-Z][a-zA-Z1-9<>]+ +([a-zA-Z][a-zA-Z1-9]+) *" +
-                                        @"\([a-zA-Z1-9<> ,]+\) *{", RegexOptions.Compiled);
+            MethodRegex = new Regex(MethodRegexString, RegexOptions.Compiled);
 
         }
 
-        private static readonly Regex MethodNameRegex;
+        private static readonly Regex MethodRegex;
 
         public override IReadOnlyCollection<JavaMethod> Parse(JavaClass code)
         {
             if (code == null)
                 throw new NullReferenceException("code");
-
+            
             var classSource = code.NormalizedSource;
             var methodSources = ParseMethodSources(classSource);
-            var methods = methodSources.Select(s => new JavaMethod(ParseMethodName(s), code, s)).ToList();
+            var methods =
+                methodSources.
+                    Select(s => new JavaMethod(ParseMethodName(s), ParseParameters(s), s, code)).
+                    ToList();
             return methods.AsReadOnly();
         }
 
-        private IEnumerable<string> ParseMethodSources(string sources)
+        private static IEnumerable<IMethodParameterInfo> ParseParameters(string methodSource)
+        {
+            var parametersString = MethodRegex.Matches(methodSource)[0].Groups[2].Value;
+            if (string.IsNullOrWhiteSpace(parametersString))
+                return Enumerable.Empty<IMethodParameterInfo>();
+            var parameterSources = parametersString.Split(',').Select(s => s.Trim(' '));
+            var parameters = new List<IMethodParameterInfo>();
+            foreach (var parameterSource in parameterSources)
+            {
+                var typeAndParameterName = parameterSource.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+                if (typeAndParameterName.Length != 2)
+                    throw new ParsingException("Method parameter parsing error.");
+                parameters.Add(new JavaMethodParameter(typeAndParameterName[1], parameterSource));
+            }
+            return parameters;
+        }
+
+        private IEnumerable<string> ParseMethodSources(string classSources)
         {
             var methodSources = new List<string>();
 
-            var matches = MethodNameRegex.Matches(sources);
+            var matches = MethodRegex.Matches(classSources);
             foreach (var match in matches.Cast<Match>())
             {
                 var startMethodIndex = match.Index;
-                var methodOpeningBracketIndex = sources.IndexOf('{', startMethodIndex);
+                var methodOpeningBracketIndex = classSources.IndexOf('{', startMethodIndex);
                 if (methodOpeningBracketIndex == -1)
                     throw new ParsingException("No opening bracket after method declaration.");
-                var methodClosingBracketIndex = FindClosingBracketIndex(sources, "{", "}", methodOpeningBracketIndex);
+                var methodClosingBracketIndex = FindClosingBracketIndex(classSources, "{", "}", methodOpeningBracketIndex);
                 if (methodClosingBracketIndex == -1)
                     throw new ParsingException("No closing bracket for method.");
-                var methodSource = sources.Substring(startMethodIndex, methodClosingBracketIndex - startMethodIndex + 1);
+                var methodSource = classSources.Substring(startMethodIndex, methodClosingBracketIndex - startMethodIndex + 1);
                 methodSources.Add(methodSource);
             }
             return methodSources;
@@ -56,7 +81,7 @@ namespace CodeMetricsCalculator.Parsers.Java
 
         private static string ParseMethodName(string methodSource)
         {
-            return MethodNameRegex.Matches(methodSource)[0].Groups[1].Value;
+            return MethodRegex.Matches(methodSource)[0].Groups[1].Value;
         }
     }
 }
