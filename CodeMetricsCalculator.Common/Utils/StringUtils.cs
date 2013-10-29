da525462d43070aea7751938448d8dd7d061a6d2
antonsapognikov <anton.sapognikov@gmail.com>
2013-10-29 23:00:37 +0400

    Adding the ability to normalize the code

---
+++ CodeMetricsCalculator.Common/Utils/StringUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeMetricsCalculator.Common.Utils
+{
+    public static class StringUtils
+    {
+        private const string QuotesFormat = "\"{0}\"";
+        
+        public static string Quotes(this string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("value");
+            return string.Format(QuotesFormat, value);
+        }
+    }
+}
--- CodeMetricsCalculator.Parsers.Java/CodeInfo/JavaCode.cs
+++ CodeMetricsCalculator.Parsers.Java/CodeInfo/JavaCode.cs
@@ -1,9 +1,19 @@
-﻿using CodeMetricsCalculator.Parsers.CodeInfo;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using CodeMetricsCalculator.Common.Utils;
+using CodeMetricsCalculator.Parsers.CodeInfo;
 
 namespace CodeMetricsCalculator.Parsers.Java.CodeInfo
 {
     public class JavaCode : MemberInfo
     {
+        private const string StringLiteralPattern = "\"[^\"]*\"";
+        private const string MultilineCommentPattern = "/\\*[^\\*/]*\\*/";       
+        private readonly string _inlineCommentPattern = string.Format("//[^{0}]*{0}", Environment.NewLine);
+
+        private readonly Dictionary<Guid, string> _stringLiterals = new Dictionary<Guid, string>(); 
+
         public JavaCode(string originalSource)
             : base(originalSource)
         {
@@ -11,9 +21,22 @@ namespace CodeMetricsCalculator.Parsers.Java.CodeInfo
 
         protected override string NormalizeSource(string originalSource)
         {
-            //todo: удалить закомментированный код, очистить строковые литералы, 
-            //todo: удалить лишние пробелы, пустые строки и т.п.
-            return originalSource;
+            //Replacing string literals...
+            var normalizedSource = Regex.Replace(originalSource, StringLiteralPattern, match =>
+                {
+                    if (_stringLiterals.ContainsValue(match.Value))
+                    {
+                        return _stringLiterals.GetKey(match.Value).ToString().Quotes();
+                    }
+                    var guid = Guid.NewGuid();
+                    _stringLiterals.Add(guid, match.Value);
+                    return guid.ToString().Quotes();
+                });
+            //Removing inline comments...
+            normalizedSource = Regex.Replace(normalizedSource, _inlineCommentPattern, Environment.NewLine);
+            //Removing multiline comments...
+            normalizedSource = Regex.Replace(normalizedSource, MultilineCommentPattern, string.Empty);
+            return normalizedSource;
         }
     }
 }
