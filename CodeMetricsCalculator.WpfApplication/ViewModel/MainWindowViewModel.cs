cb7d1cc47a4b8290d96b9a1d66f94bad91c94643
RavingRabbit <RavingDev@gmail.com>
2013-11-01 22:43:50 +0300

    Add code for testing.

--- CodeMetricsCalculator.WpfApplication/ViewModel/MainWindowViewModel.cs
+++ CodeMetricsCalculator.WpfApplication/ViewModel/MainWindowViewModel.cs
@@ -1,7 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using CodeMetricsCalculator.Common.UI.ViewModel;
 using CodeMetricsCalculator.Common.UI.ViewModel.Base;
+using CodeMetricsCalculator.Parsers.CodeInfo;
+using CodeMetricsCalculator.Parsers.Java;
+using CodeMetricsCalculator.Parsers.Java.CodeInfo;
 using Microsoft.Win32;
 
 namespace CodeMetricsCalculator.WpfApplication.ViewModel
@@ -12,7 +21,8 @@ namespace CodeMetricsCalculator.WpfApplication.ViewModel
         private bool _isResultVisible;
         private string _fileName;
         private string _log;
-        private string _result; 
+        private string _result;
+        private IReadOnlyCollection<IClassInfo> _classes;
         private ICommand _openFileCommand;
 
         private const string JavaSourceFilter = "Java source files (*.java)|*.java|All files (*.*)|*.*";
@@ -75,7 +85,7 @@ namespace CodeMetricsCalculator.WpfApplication.ViewModel
         public MainWindowViewModel()
         {
             //Test values
-            Log = "Exception: File not found!";
+            Log = "Waiting for file...";
             IsLogVisible = true;
         }
 
@@ -86,7 +96,68 @@ namespace CodeMetricsCalculator.WpfApplication.ViewModel
             if (result.Value)
             {
                 FileName = dialog.FileName;
+                LoadAndParseFile();
             }
+        }
+
+        private async void LoadAndParseFile()
+        {
+            try
+            {
+                string source;
+                Log = "Reading file...";
+                using (var sr = new StreamReader(File.OpenRead(FileName)))
+                {
+                    source = await sr.ReadToEndAsync();
+                }
+                var code = new JavaCode(source);
+                IsResultVisible = true;
+                Log = "Parsing classes...";
+                var parseClassesTask = new Task<IReadOnlyCollection<IClassInfo>>(() => new JavaClassParser().Parse(code));
+                parseClassesTask.Start();
+                _classes = await parseClassesTask;
+                //Result = _classes.Select(@class => @class.Name).Aggregate((s, s1) => s + Environment.NewLine + s1);
+                Log = string.Format("Parsed {0} classes.", _classes.Count);
+
+                Log = "Parsing expressions...";
+                var parseExpressionsTask = new Task<List<IExpressionInfo>>(() => _classes.Select(@class => @class.GetMethods())
+                 .Aggregate(AggregateMethods)
+                 .Select(info => info.GetBody())
+                 .Select(info => info.GetExpressions())
+                 .Aggregate(
+                     AggregateExpressions).ToList());
+                parseExpressionsTask.Start();
+                var expressions = await parseExpressionsTask;
+                Log = string.Format("Parsed {0} expressions.", expressions.Count);
+                var sb = new StringBuilder();
+                sb.AppendLine("All expressions: ");
+                foreach (var expressionInfo in expressions)
+                {
+                    sb.AppendLine(expressionInfo.NormalizedSource);
+                }
+                Result = sb.ToString();
+            }
+            catch (Exception e)
+            {
+                Log = e.Message;
+            }
+        }
+
+        private static IReadOnlyCollection<IExpressionInfo> AggregateExpressions(IReadOnlyCollection<IExpressionInfo> readOnlyCollection, IReadOnlyCollection<IExpressionInfo> expressionInfos)
+        {
+            var list = new List<IExpressionInfo>();
+            list.AddRange(readOnlyCollection);
+            list.AddRange(expressionInfos);
+            return new ReadOnlyCollection<IExpressionInfo>(list);
+        }
+
+        private static IReadOnlyCollection<IMethodInfo> AggregateMethods(IReadOnlyCollection<IMethodInfo> readOnlyCollection,
+            IReadOnlyCollection<IMethodInfo> onlyCollection)
+        {
+            var list = new List<IMethodInfo>();
+            list.AddRange(readOnlyCollection);
+            list.AddRange(onlyCollection);
+            return new ReadOnlyCollection<IMethodInfo>(list);
         }
     }
 }
