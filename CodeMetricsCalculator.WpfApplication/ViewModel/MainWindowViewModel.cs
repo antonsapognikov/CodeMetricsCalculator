29dd1dfed0d3cb3c28c62e81f396ae4b10baddf4
antonsapognikov <anton.sapognikov@gmail.com>
2013-11-01 22:42:09 +0400

    Update "Log" and "Result" GUI

--- CodeMetricsCalculator.WpfApplication/ViewModel/MainWindowViewModel.cs
+++ CodeMetricsCalculator.WpfApplication/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Windows.Input;
-using CodeMetricsCalculator.Common.UI.ViewModel;
+﻿using System.Windows.Input;
 using CodeMetricsCalculator.Common.UI.ViewModel.Base;
 using Microsoft.Win32;
 
@@ -8,34 +6,12 @@ namespace CodeMetricsCalculator.WpfApplication.ViewModel
 {
     public class MainWindowViewModel : UIViewModel
     {
-        private bool _isLogVisible;
-        private bool _isResultVisible;
         private string _fileName;
         private string _log;
         private string _result; 
         private ICommand _openFileCommand;
 
         private const string JavaSourceFilter = "Java source files (*.java)|*.java|All files (*.*)|*.*";
-
-        public bool IsLogVisible
-        {
-            get { return _isLogVisible; }
-            set
-            {
-                _isLogVisible = value;
-                OnPropertyChanged(() => IsLogVisible);
-            }
-        }
-
-        public bool IsResultVisible
-        {
-            get { return _isResultVisible; }
-            set
-            {
-                _isResultVisible = value;
-                OnPropertyChanged(() => IsResultVisible);
-            }
-        }
 
         public string FileName
         {
@@ -70,13 +46,6 @@ namespace CodeMetricsCalculator.WpfApplication.ViewModel
         public ICommand OpenFileCommand
         {
             get { return _openFileCommand ?? (_openFileCommand = CreateCommand<object>(parameter => OnCreateFile())); }
-        }
-
-        public MainWindowViewModel()
-        {
-            //Test values
-            Log = "Exception: File not found!";
-            IsLogVisible = true;
         }
 
         private void OnCreateFile()
