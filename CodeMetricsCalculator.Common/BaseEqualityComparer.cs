a7a62723052ff867bb78d54d28fdeb1377678765
Sapozhnikau <anton.sapognikov@gmail.com>
2013-11-29 23:41:38 +0300

    Variable parsing

---
+++ CodeMetricsCalculator.Common/BaseEqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeMetricsCalculator.Common
+{
+    public class BaseEqualityComparer<T> : EqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> _equals;
+        private readonly Func<T, int> _hashCode;
+        
+        public BaseEqualityComparer(Func<T, T, bool> equals, Func<T, int> hashCode)
+        {
+            if (equals == null)
+                throw new ArgumentNullException("equals");
+            if (hashCode == null)
+                throw new ArgumentNullException("hashCode");
+            _equals = equals;
+            _hashCode = hashCode;
+        }
+        
+        public override bool Equals(T x, T y)
+        {
+            return _equals(x, y);
+        }
+
+        public override int GetHashCode(T obj)
+        {
+            return _hashCode(obj);
+        }
+    }
+}
--- CodeMetricsCalculator.Parsers.Java/JavaVariablesParser.cs
+++ CodeMetricsCalculator.Parsers.Java/JavaVariablesParser.cs
@@ -1,25 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using CodeMetricsCalculator.Parsers.CodeInfo;
+using CodeMetricsCalculator.Common;
 using CodeMetricsCalculator.Parsers.Java.CodeInfo;
 
 namespace CodeMetricsCalculator.Parsers.Java
 {
     internal class JavaVariablesParser : JavaCodeParser<JavaMethod, IReadOnlyDictionary<JavaVariable, int>>
     {
-        private const string JavaIdentifierPattern = "[^a-zA-Z0-9_]" + "{0}" + "[^a-zA-Z0-9_]"; 
-        private const string VariableDeclaringPattern =
-            @"([a-zA-Z_][a-zA-Z0-9<,>_]*) +([a-zA-Z_][a-zA-Z0-9_]*)( *= *[a-zA-Z0-9_\+\-\*\\<,>\.\(\)" + "\" ]+)? *;";
+        private const string EmptyOrWhiteSpacePattern = @"[ \t]*";
+        private const string WhiteSpacePattern = @"[ \t]+";
+        private const string TypeIdentifierPattern = @"[a-zA-Z_][a-zA-Z0-9_]+(<[a-zA-Z0-9<,>_ ]+>)?";
+        private const string VariableIdentifierPattern = @"[a-zA-Z_][a-zA-Z0-9_]*";
+        private const string VariableValuePattern = @"[^;]+";
+        private const string BracketArgumentsPattern = @"\([^=]*\)";
+        private const string JavaIdentifierPattern = "[^a-zA-Z0-9_]" + "{0}" + "[^a-zA-Z0-9_]";
 
-        static JavaVariablesParser()
+        private static readonly string DeclarationPattern =
+            string.Format(@"{0}{2}{1}{3}{0}(={0}{4}{0})?(,{0}{3}{0}(={0}{4}{0})?)*;", EmptyOrWhiteSpacePattern, WhiteSpacePattern, TypeIdentifierPattern, VariableIdentifierPattern, VariableValuePattern);
+
+        private static readonly string DeclarationTypePattern =
+            string.Format(@"^{0}{2}{1}", EmptyOrWhiteSpacePattern, WhiteSpacePattern, TypeIdentifierPattern);
+
+        private static readonly string DeclarationVariableNamePattern =
+            string.Format(@"(^|,){0}{1}", EmptyOrWhiteSpacePattern, VariableIdentifierPattern);
+
+        private static readonly string ForeachLoopPattern =
+            string.Format(@"for{0}\({0}{2}{1}{3}{0}:{0}{3}{0}\)", EmptyOrWhiteSpacePattern, WhiteSpacePattern, TypeIdentifierPattern, VariableIdentifierPattern);
+
+        private static readonly string ForeachLoopTypePattern =
+            string.Format(@"\({0}{1}", EmptyOrWhiteSpacePattern, TypeIdentifierPattern);
+
+        private static readonly string ForeachLoopVariableNamePattern =
+            string.Format(@"{1}{0}:", EmptyOrWhiteSpacePattern, VariableIdentifierPattern);
+
+        private static readonly List<string> ReservedIdentifierPatterns = new List<string>
         {
-            VariableDeclaringRegex = new Regex(VariableDeclaringPattern, RegexOptions.Compiled);
-        }
+            CreateReservedIdentifierPattern("return"), 
+            CreateReservedIdentifierPattern("goto")
+        };  
 
         public override IReadOnlyDictionary<JavaVariable, int> Parse(JavaMethod code)
         {
@@ -48,22 +68,53 @@ namespace CodeMetricsCalculator.Parsers.Java
 
             return identifiers;
         }
-        private static readonly Regex VariableDeclaringRegex;
 
-        private IReadOnlyCollection<JavaVariable> ParseVariables(string methodSource)
+        private IEnumerable<JavaVariable> ParseVariables(string methodSource)
         {
-            var variables = new List<JavaVariable>();
-            var matches = VariableDeclaringRegex.Matches(methodSource).Cast<Match>();
-            foreach (var match in matches)
-            {
-                var variableDeclaring = match.Value;
-                if (variableDeclaring.StartsWith("return") || variableDeclaring.StartsWith("goto"))
-                    continue;
-                var groups = match.Groups;
-                var type = new JavaType(match.Groups[1].Value);
-                variables.Add(new JavaVariable(type, match.Groups[2].Value, match.Value));
-            }
-            return variables.AsReadOnly();
-        } 
+            return ParseDeclarations(methodSource)
+                .Concat(ParseForeachLoops(methodSource))
+                .Distinct(new BaseEqualityComparer<JavaVariable>((first, second) => first.Name == second.Name, variable => variable.Name.GetHashCode()))
+                .ToList()
+                .AsReadOnly();
+        }
+
+        private IEnumerable<JavaVariable> ParseDeclarations(string normilizedSource)
+        {
+            return Regex.Matches(normilizedSource, DeclarationPattern).Cast<Match>()
+                .Select(match => match.Value)
+                .Where(value => ReservedIdentifierPatterns.All(pattern => !Regex.IsMatch(value, pattern)))
+                .SelectMany(ParseDeclaration)               
+                .ToList();
+        }
+
+        private IEnumerable<JavaVariable> ParseForeachLoops(string normilizedSource)
+        {
+            return Regex.Matches(normilizedSource, ForeachLoopPattern).Cast<Match>()
+                .Select(match => ParseForeachLoop(match.Value))
+                .ToList();
+        }
+
+        private IEnumerable<JavaVariable> ParseDeclaration(string declaration)
+        {
+            string type = Regex.Match(declaration, DeclarationTypePattern).Value.TrimStart(' ').TrimEnd(' ');
+            string withoutType = Regex.Replace(declaration, DeclarationTypePattern, string.Empty);
+            string withoutBrackets = Regex.Replace(withoutType, BracketArgumentsPattern, string.Empty);
+            List<string> names = Regex.Matches(withoutBrackets, DeclarationVariableNamePattern).Cast<Match>()
+                .Select(match => match.Value.TrimStart(',', ' '))
+                .ToList();
+            return names.Select(value => new JavaVariable(new JavaType(type), value, declaration)).ToList();
+        }
+
+        private JavaVariable ParseForeachLoop(string loop)
+        {
+            string type = Regex.Match(loop, ForeachLoopTypePattern).Value.TrimStart('(', ' ');
+            string name = Regex.Match(loop, ForeachLoopVariableNamePattern).Value.TrimEnd(' ', ':');
+            return new JavaVariable(new JavaType(type), name, loop);
+        }
+
+        private static string CreateReservedIdentifierPattern(string identifier)
+        {
+            return string.Format("^{0}{2}{1}{3}{0};$", EmptyOrWhiteSpacePattern, WhiteSpacePattern, identifier, VariableValuePattern);
+        }
     }
 }
